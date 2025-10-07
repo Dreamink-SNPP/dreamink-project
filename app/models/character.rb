7734115ad6ddbecf5ba13f10f06f7b0e9ebb9b40
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 12:24:04 -0300

    Add validations, relationship, callbacks on character.rb

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,21 @@
 class Character < ApplicationRecord
   belongs_to :project
+
+  has_one :internal_trait, class_name: 'CharacterInternalTrait', dependent: :destroy
+  has_one :external_trait, class_name: 'CharacterExternalTrait', dependent: :destroy
+
+  accepts_nested_attributes_for :internal_trait, :external_trait
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :name, uniqueness: { scope: :project_id, message: "ya existe en este proyecto" }
+
+  # Callbacks:
+  after_create :create_default_traits
+
+  private
+
+  def create_default_traits
+    create_internal_trait! unless internal_trait
+    create_external_trait! unless external_trait
+  end
 end
