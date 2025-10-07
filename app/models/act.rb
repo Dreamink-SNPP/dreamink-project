b41b97973b371f2f1832dfd3218a75f49fa087d4
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 12:14:35 -0300

    Add validation to title on act.rb

--- app/models/act.rb
+++ app/models/act.rb
@@ -2,4 +2,8 @@ class Act < ApplicationRecord
   belongs_to :project
   has_many :sequences, -> { order(position: :asc) }, dependent: :destroy
   has_many :scenes, through: :sequences
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :position, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :position, uniqueness: { scope: :project_id }
 end
