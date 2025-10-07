75000b9692d75d077adde7800b2fd19e7c6d828a
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 15:10:26 -0300

    Add sequence controller test

---
+++ test/controllers/sequences_controller_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class SequencesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get sequences_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get sequences_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get sequences_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get sequences_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get sequences_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get sequences_destroy_url
+    assert_response :success
+  end
+end
