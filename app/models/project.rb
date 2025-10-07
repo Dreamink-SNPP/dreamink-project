52433057817792b4832a94207ecb11352a3c9553
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 12:08:44 -0300

    Add validations on project.rb for different models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ApplicationRecord
   belongs_to :user
+  # Validations:
+  has_many :acts, -> { order(position: :asc) }, dependent: :destroy
+  has_many :sequences, through: :acts
+  has_many :scenes, through: :sequences
+  has_many :characters, dependent: :destroy
+  has_many :locations, dependent: :destroy
+  has_many :ideas, -> { order(created_at: :desc) }, dependent: :destroy
 end
