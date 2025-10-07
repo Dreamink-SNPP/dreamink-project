ae0c7639aa7d87c594332258f2be82c02aa4f0d6
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 15:22:00 -0300

    Add set method for Character and certain params
    These params are internal and external traits attributes.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -39,4 +39,31 @@ class CharactersController < ApplicationController include ProjectAuthorization
     @character.destroy
     redirect_to project_characters_path(@project), notice: "Personaje eliminado exitosamente"
   end
+
+  private
+
+  def set_character
+    @character = @project.characters.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(
+      :name,
+      internal_trait_attributes: [
+        :id, :skills, :religion, :spirituality, :identity, :beliefs,
+        :mental_programs, :ethics, :sexuality, :main_motivation,
+        :friendship_relations, :conversation_focus, :self_awareness_level,
+        :values_priorities, :time_management, :artistic_inclinations,
+        :heroes_models, :political_ideas, :authority_relationship,
+        :vices, :temporal_location, :food_preferences, :habits,
+        :peculiarities, :hobbies, :charitable_activities
+      ],
+      external_trait_attributes: [
+        :id, :general_appearance, :detailed_appearance, :medical_history,
+        :family_structure, :education, :profession, :legal_situation,
+        :economic_situation, :important_possessions, :residence_type,
+        :usual_location, :pets
+      ]
+    )
+  end
 end
