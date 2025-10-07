0ac148f8c89d1b8f560bd446814dd551b8e8dac5
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 12:49:19 -0300

    Add email->email-address aliases on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_many :projects, dependent: :destroy
+
+  alias_attribute :email, :email_address
+
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
