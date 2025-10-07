d76c2c9228dc0274483a2a24f28cffeecabce216
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 15:30:49 -0300

    Add basic controller for structures_controller.rb
    These basics are reorder sequences, scenes and even acts.

--- app/controllers/structures_controller.rb
+++ app/controllers/structures_controller.rb
@@ -1,7 +1,43 @@
 class StructuresController < ApplicationController
+  include ProjectAuthorization
+
   def show
+    @acts = @project.acts.includes(sequences: :scenes).ordered
   end
 
   def reorder
+    case params[:type]
+    when 'act'
+      reorder_acts
+    when 'sequence'
+      reorder_sequences
+    when 'scene'
+      reorder_scenes
+    else
+      head :bad_request
+    end
+  end
+
+  private
+
+  def reorder_acts
+    params[:acts].each_with_index do |act_id, index|
+      Act.find(act_id).update(position: index)
+    end
+    head :ok
+  end
+
+  def reorder_sequences
+    params[:sequences].each_with_index do |sequence_id, index|
+      Sequence.find(sequence_id).update(position: index)
+    end
+    head :ok
+  end
+
+  def reorder_scenes
+    params[:scenes].each_with_index do |scene_id, index|
+      Scene.find(scene_id).update(position: index)
+    end
+    head :ok
   end
 end
