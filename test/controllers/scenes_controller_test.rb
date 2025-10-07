8b318b0b6d28d916d81f0e998e93464b0cbf49be
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 15:14:33 -0300

    Add test Scene default file

---
+++ test/controllers/scenes_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class ScenesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get scenes_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get scenes_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get scenes_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get scenes_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get scenes_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get scenes_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get scenes_destroy_url
+    assert_response :success
+  end
+end
