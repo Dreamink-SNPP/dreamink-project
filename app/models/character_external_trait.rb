426f65c599e198fadfe0fcc1a303168e3dc7ccba
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 12:25:53 -0300

    Add length validations on character_external_trait.rb

--- app/models/character_external_trait.rb
+++ app/models/character_external_trait.rb
@@ -1,3 +1,13 @@
 class CharacterExternalTrait < ApplicationRecord
   belongs_to :character
+
+  validates :medical_history, length: { maximum: 500 }, allow_blank: true
+  validates :education, length: { maximum: 500 }, allow_blank: true
+  validates :profession, length: { maximum: 300 }, allow_blank: true
+  validates :legal_situation, length: { maximum: 300 }, allow_blank: true
+  validates :economic_situation, length: { maximum: 300 }, allow_blank: true
+  validates :important_possessions, length: { maximum: 500 }, allow_blank: true
+  validates :residence_type, length: { maximum: 300 }, allow_blank: true
+  validates :usual_location, length: { maximum: 300 }, allow_blank: true
+  validates :pets, length: { maximum: 300 }, allow_blank: true
 end
