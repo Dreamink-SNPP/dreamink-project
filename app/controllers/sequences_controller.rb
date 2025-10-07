73a97e4200d140fe4305ef3e4685c4f83a9b711f
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 15:12:22 -0300

    Add basic CRUD on sequences_controller.rb

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -1,19 +1,38 @@
-class SequencesController < ApplicationController
+class SequencesController < ApplicationController include ProjectAuthorization
+  before_action :set_sequence, only: [:edit, :update, :destroy, :move]
+  before_action :set_act, only: [:new, :create]
+
   def index
+    @sequences = @project.sequences.includes(:act).order('acts.position, sequences.position')
   end
 
   def new
+    @sequence = @act.sequences.build
   end
 
   def create
+    @sequence = @act.sequences.build(sequence_params)
+
+    if @sequence.save
+      redirect_to project_structure_path(@project), notice: "Secuencia creada exitosamente"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @sequence.update(sequence_params)
+      redirect_to project_structure_path(@project), notice: "Secuencia actualizada exitosamente"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @sequence.destroy
+    redirect_to project_structure_path(@project), notice: "Secuencia eliminada exitosamente"
   end
 end
