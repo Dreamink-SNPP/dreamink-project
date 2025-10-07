aafe3998abca0d42c3c65dad2a37a333e65a4476
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 14:59:56 -0300

    Add additional parameters on turbo in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :turbo_frame_request_variant
+
+  private
+
+  def turbo_frame_request_variant
+    request.variant = :turbo_frame if turbo_frame_request?
+  end
 end
