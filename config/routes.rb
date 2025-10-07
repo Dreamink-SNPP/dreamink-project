df36330243a1d021ba438325b0e3045b9c0f4e09
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 15:04:54 -0300

    Add GET project methods on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "projects/index"
+  get "projects/show"
+  get "projects/new"
+  get "projects/create"
+  get "projects/edit"
+  get "projects/update"
+  get "projects/destroy"
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
