1ca64a849b1792d9955d09a15c4bcb7e352d2f34
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 11:27:11 -0300

    Add email and password validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
