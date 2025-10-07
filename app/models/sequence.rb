ec0543d26f67e087d34333b881c51839d21c54db
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 12:18:37 -0300

    Add validations, callbacks and scopes on sequence.rb

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -1,3 +1,23 @@
 class Sequence < ApplicationRecord
   belongs_to :act
+
+  has_one :project, through: :act
+  has_many :scenes, -> { order(position: :asc) }, dependent: :destroy
+
+  # Validations:
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :position, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :position, uniqueness: { scope: :act_id }
+
+  # Callbacks:
+  before_validation :set_position, on: :create
+
+  # Scopes:
+  scope :ordered, -> { order(position: :asc) }
+
+  private
+
+  def set_position
+    self.position ||= act.sequences.maximum(:position).to_i + 1
+  end
 end
