8b28a30cb64fe242c33825f9a15b50cb5b76c18d
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 12:09:49 -0300

    Add each user owns its project on project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,6 @@ class Project < ApplicationRecord
   has_many :characters, dependent: :destroy
   has_many :locations, dependent: :destroy
   has_many :ideas, -> { order(created_at: :desc) }, dependent: :destroy
+  # Privacy scope:
+  scope :for_user, ->(user) { where(user: user) }
 end
