15a8e42eb278090e20085b2f125ccf925f3a7fe5
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-07 12:31:32 -0300

    Add validations and scopes on idea.rb

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,12 @@
 class Idea < ApplicationRecord
   belongs_to :project
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, presence: true
+
+  # Scopes
+  scope :recent, -> { order(created_at: :desc) }
+  scope :search, ->(query) { where("title ILIKE ? OR description ILIKE ?", "%#{query}%", "%#{query}%") }
+  scope :tagged_with, ->(tag) { where("tags ILIKE ?", "%#{tag}%") }
+
 end
