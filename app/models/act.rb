624d2e2421ee90eda763dd21fc2833ca464583c6
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-08 16:16:50 -0300

    Add acts_as_list scope on act.rb

--- app/models/act.rb
+++ app/models/act.rb
@@ -5,6 +5,8 @@ class Act < ApplicationRecord
   has_many :sequences, -> { order(position: :asc) }, dependent: :destroy
   has_many :scenes, through: :sequences
 
+  acts_as_list scope: :project
+
   validates :title, presence: true, length: { maximum: 100 }
   validates :position, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :position, uniqueness: { scope: :project_id }
