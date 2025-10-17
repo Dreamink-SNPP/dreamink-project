651685e373fd8e12f1b41d826f75f4801be564d9
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-17 19:20:38 -0300

    Add edit_modal get on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Rails.application.routes.draw do
     resources :scenes do
       member do
         get :new_modal
+        get :edit_modal
       end
       collection do
         get :by_location # Filter scenes by locations
