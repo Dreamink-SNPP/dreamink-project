fbbe7847107a72c922bd8dc33e6a97f3e87691cd
Fernando Cardozo <nuevo_10000@outlook.com>
2025-10-27 08:49:25 -0300

    Add move_to_act method to Sequence model
    - Allows moving sequences between acts - Updates position using
    acts_as_list methods - Automatically syncs project_id and act_id for
    child scenes - Uses transaction for atomicity - Returns boolean for
    success/failure

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -8,16 +8,45 @@ class Sequence < ApplicationRecord
 
   acts_as_list scope: :act
 
-  # Validations:
   validates :title, presence: true, length: { maximum: 100 }
   validates :position, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  # Callbacks:
   before_validation :set_position, on: :create
   before_validation :sync_project_reference
 
-  # Scopes:
   scope :ordered, -> { order(position: :asc) }
+
+  def move_to_act(new_act, new_position: nil)
+    return false if new_act.nil?
+    return true if act_id == new_act.id
+
+    ActiveRecord::Base.transaction do
+      remove_from_list
+
+      self.act = new_act
+      self.project_id = new_act.project_id
+
+      if new_position
+        self.position = new_position
+      else
+        self.position = new_act.sequences.maximum(:position).to_i + 1
+      end
+
+      save!
+
+      scenes.update_all(
+        act_id: new_act.id,
+        project_id: new_act.project_id
+      )
+
+      add_to_list(position)
+    end
+
+    true
+  rescue ActiveRecord::RecordInvalid => e
+    Rails.logger.error "Error moving sequence: #{e.message}"
+    false
+  end
 
   private
 
